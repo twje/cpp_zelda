8fed8543cf1ce02122cf905e139fac49a5802376
Thomas <11610634+twje@users.noreply.github.com>
2023-07-14 05:17:52 +1000

    Add debug text render

---
+++ src/Debug.cpp
@@ -0,0 +1,25 @@
+#include "Debug.h"
+
+void Debug(sf::RenderWindow &window, const std::string &info, uint16_t x, uint16_t y)
+{
+    sf::Font font;
+    if (!font.loadFromFile("../graphics/font/joystix.ttf"))
+    {
+        // Error handling for font loading
+        return;
+    }
+
+    // Text
+    sf::Text text(font, info);
+    text.setCharacterSize(24);
+    text.setPosition(sf::Vector2f(x, y));
+
+    // Text Background
+    sf::FloatRect textBounds = text.getGlobalBounds();
+    sf::RectangleShape background(sf::Vector2f(textBounds.width, textBounds.height));
+    background.setPosition(sf::Vector2f(textBounds.left, textBounds.top));
+    background.setFillColor(sf::Color::Black);
+
+    window.draw(background);
+    window.draw(text);
+}
---
+++ src/Debug.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+void Debug(sf::RenderWindow &window, const std::string &info, uint16_t x = 10, uint16_t y = 10);
