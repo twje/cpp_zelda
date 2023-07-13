fe98a1b84e727ce978c7bf865be247b622ba0a6d
Thomas <11610634+twje@users.noreply.github.com>
2023-07-13 21:16:37 +1000

    Add unit test

--- tests/Example.cpp
+++ tests/Example.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
 
-TEST(ExampleTests, DemonstrateGTestMacros)
+TEST(ExampleTests, GridCoordToIndex)
 {
-    EXPECT_TRUE(true);
+    size_t rows = 20;
+    size_t cols = 20;
+    size_t count = 0;
+
+    for (size_t y = 0; y < rows; y++)
+    {
+        for (size_t x = 0; x < cols; x++)
+        {
+            size_t index = x + (y * cols);
+            EXPECT_EQ(index, count);
+            count += 1;
+        }
+    }
 }
