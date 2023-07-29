b86c55b887ea8ece4b85470a880b57ccdd56c65d
Thomas <11610634+twje@users.noreply.github.com>
2023-07-29 12:47:50 +1000

    Animation bug fix

--- src/Core/Animation.cpp
+++ src/Core/Animation.cpp
@@ -1,29 +1,6 @@
 #include "Animation.h"
 #include <cassert>
 
-AnimationSequence::AnimationSequence(float framesPerSecond)
-    : mFrameTime(1.0f / framesPerSecond),
-      mElapsedTime(0),
-      mFrameIndex(0)
-{
-}
-
-void AnimationSequence::Update(const sf::Time &timestamp)
-{
-    mElapsedTime += timestamp.asSeconds();
-    if (mElapsedTime > mFrameTime)
-    {
-        mElapsedTime -= mFrameTime;
-        mFrameIndex = (mFrameIndex + 1) % GetFrameCount();
-    }
-}
-
-TextureAnimationSequence::TextureAnimationSequence(uint8_t framesPerSecond, const TextureVector &textures)
-    : AnimationSequence(framesPerSecond),
-      mTextures(textures)
-{
-}
-
 Animation::Animation()
     : mCurrentSequence(nullptr)
 {
