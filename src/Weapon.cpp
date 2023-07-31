6589e182c0d929d2fbd37d3ffa0500cc1132dca9
Thomas <11610634+twje@users.noreply.github.com>
2023-08-01 04:19:46 +1000

    Split out Weapon class into cpp file

--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -1,1 +1,42 @@
 #include "Weapon.h"
+
+#include <sstream>
+
+#include <SFML/Graphics.hpp>
+
+// Core
+#include "Core/RectUtils.h"
+#include "Core/TextureManager.h"
+
+// Game
+#include "Constants.h"
+
+Weapon::Weapon(const Player &player)
+    : Sprite()
+{
+    std::string direction = player.GetDirection();
+
+    // Init texture
+    std::ostringstream oss;
+    oss << player.GetWeapon() << "_" << direction;
+    std::string textureID = oss.str();
+    setTexture(*TextureManager::GetInstance().GetTexture(textureID), true);
+
+    // Set position
+    if (direction == "right")
+    {
+        setPosition(GetRectMidRight(player.GetGlobalBounds()) - GetRectMidLeft(GetLocalBounds()) + sf::Vector2f(0, 16));
+    }
+    else if (direction == "left")
+    {
+        setPosition(GetRectMidLeft(player.GetGlobalBounds()) - GetRectMidRight(GetLocalBounds()) + sf::Vector2f(0, 16));
+    }
+    else if (direction == "down")
+    {
+        setPosition(GetRectMidBottom(player.GetGlobalBounds()) - GetRectMidTop(GetLocalBounds()) + sf::Vector2f(-10, 0));
+    }
+    else
+    {
+        setPosition(GetRectMidTop(player.GetGlobalBounds()) - GetRectMidBottom(GetLocalBounds()) + sf::Vector2f(-10, 0));
+    }
+}
--- src/Weapon.h
+++ src/Weapon.h
@@ -1,48 +1,13 @@
 #pragma once
 
-#include <sstream>
-
-#include <SFML/Graphics.hpp>
-
 // Core
-#include "Core/RectUtils.h"
-#include "Core/TextureManager.h"
+#include "Core/Sprite.h"
 
 // Game
-#include "Core/Sprite.h"
-#include "Constants.h"
 #include "Player.h"
 
 class Weapon : public Sprite
 {
 public:
-    Weapon(const Player &player)
-        : Sprite()
-    {
-        std::string direction = player.GetDirection();
-
-        // Init texture
-        std::ostringstream oss;
-        oss << player.GetWeapon() << "_" << direction;
-        std::string textureID = oss.str();
-        setTexture(*TextureManager::GetInstance().GetTexture(textureID), true);
-
-        // Set position
-        if (direction == "right")
-        {
-            setPosition(GetRectMidRight(player.GetGlobalBounds()) - GetRectMidLeft(GetLocalBounds()) + sf::Vector2f(0, 16));
-        }
-        else if (direction == "left")
-        {
-            setPosition(GetRectMidLeft(player.GetGlobalBounds()) - GetRectMidRight(GetLocalBounds()) + sf::Vector2f(0, 16));
-        }
-        else if (direction == "down")
-        {
-            setPosition(GetRectMidBottom(player.GetGlobalBounds()) - GetRectMidTop(GetLocalBounds()) + sf::Vector2f(-10, 0));
-        }
-        else
-        {
-            setPosition(GetRectMidTop(player.GetGlobalBounds()) - GetRectMidBottom(GetLocalBounds()) + sf::Vector2f(-10, 0));
-        }
-    }
+    Weapon(const Player &player);
 };
