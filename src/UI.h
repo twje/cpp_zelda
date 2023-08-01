4c2d9176c3478f45d17222e6423b36abd2f72c12
Thomas <11610634+twje@users.noreply.github.com>
2023-08-01 20:50:23 +1000

    Scaffold UI class

--- src/UI.h
+++ src/UI.h
@@ -12,6 +12,20 @@ class UI : public Layer
 {
 public:
     UI(const Level &level)
+        : mLevel(level)
     {
     }
+
+    virtual void Draw(sf::RenderWindow &window)
+    {
+    }
+
+private:
+    void ShowBar() {}
+    void ShowExperience() {}
+    void SelectionBox() {}
+    void WeaponOverlay() {}
+
+private:
+    const Level &mLevel;
 };
