e4f04cc8f7784f73095c61487534b499bb5a3da5
Thomas <11610634+twje@users.noreply.github.com>
2023-08-02 20:02:07 +1000

    Moved common methods into GameObject

--- src/Core/GameObject.h
+++ src/Core/GameObject.h
@@ -2,9 +2,22 @@
 
 #include <SFML/Graphics.hpp>
 
-class GameObject
+class GameObject : private sf::Transformable
 {
 public:
+    // Hooks
     virtual void Draw(sf::RenderWindow &window) {}
-    virtual void Update(const sf::Time &timestamp){};
+    virtual void Update(const sf::Time &timestamp) {}
+    virtual sf::FloatRect GetLocalBounds() const { return sf::FloatRect(); }
+    virtual sf::FloatRect GetHitbox() const { return GetGlobalBounds(); }
+
+    // Setters
+    void SetPosition(const sf::Vector2f &position) { setPosition(position); }
+    void Move(const sf::Vector2f &offset) { move(offset); }
+
+    // Getters
+    sf::Vector2f GetSize() const { return GetGlobalBounds().getSize(); }
+    sf::Vector2f GetCenter() const { return getPosition() + 0.5f * GetSize(); }
+    sf::FloatRect GetGlobalBounds() const { return getTransform().transformRect(GetLocalBounds()); }
+    const sf::Transform &GetTransform() const { return getTransform(); };
 };
--- src/Core/Sprite.h
+++ src/Core/Sprite.h
@@ -10,7 +10,7 @@
 // Forward class declaration
 class SpriteGroup;
 
-class Sprite : public GameObject, public sf::Transformable
+class Sprite : public GameObject
 {
     using SpriteGroupVector = std::vector<SpriteGroup *>;
     friend SpriteGroup;
@@ -19,21 +19,15 @@ public:
     Sprite(const sf::Texture &texture);
     Sprite();
 
-    void Draw(sf::RenderWindow &window) override { window.draw(mSprite, getTransform()); }
+    void Draw(sf::RenderWindow &window) override { window.draw(mSprite, GetTransform()); }
     void Kill();
 
     // Setters
     void SetTexture(const sf::Texture &texture, bool resetRect = false) { mSprite.setTexture(texture, resetRect); }
     void SetTextureRect(const sf::IntRect &rectangle) { mSprite.setTextureRect(rectangle); }
-    void SetPosition(const sf::Vector2f &position) { setPosition(position); }
-    void Move(const sf::Vector2f &offset) { move(offset); }
 
     // Getters
-    sf::Vector2f GetSize() const { return GetGlobalBounds().getSize(); }
-    sf::Vector2f GetCenter() const { return getPosition() + 0.5f * GetSize(); }
-    sf::FloatRect GetGlobalBounds() const { return getTransform().transformRect(mSprite.getLocalBounds()); }
-    sf::FloatRect GetLocalBounds() const { return mSprite.getLocalBounds(); }
-    virtual sf::FloatRect GetHitbox() const { return GetGlobalBounds(); }
+    sf::FloatRect GetLocalBounds() const override { return mSprite.getLocalBounds(); }
 
 private:
     static const sf::Texture &GetPlaceholderTexture();
