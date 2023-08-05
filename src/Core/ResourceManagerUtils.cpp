f7f071a9964838285fbacb4b9c0f5fc67ad52611
Thomas <11610634+twje@users.noreply.github.com>
2023-08-05 19:30:06 +1000

    Replaced util class with function

--- src/Core/ResourceManagerUtils.cpp
+++ src/Core/ResourceManagerUtils.cpp
@@ -1,1 +1,24 @@
 #include "Core/ResourceManagerUtils.h"
+
+std::string NormalzeResourceID(const std::string &resourceID)
+{
+    size_t underscorePos = resourceID.find_last_of('_');
+    if (underscorePos != std::string::npos)
+    {
+        bool isAllDigits = true;
+        for (size_t i = underscorePos + 1; i < resourceID.size(); ++i)
+        {
+            if (!std::isdigit(resourceID[i]))
+            {
+                isAllDigits = false;
+                break;
+            }
+        }
+
+        if (isAllDigits)
+        {
+            return resourceID.substr(0, underscorePos);
+        }
+    }
+    return resourceID;
+}
--- src/Core/ResourceManagerUtils.h
+++ src/Core/ResourceManagerUtils.h
@@ -1,70 +1,17 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <memory>
-#include <map>
-
-#include <SFML/Graphics.hpp>
-
 #include "Core/ResourceManager.h"
 
-// Thinl about const
-
-template <typename Derived, typename T>
-class GroupResourcesByPrefixHelper
-{
-public:
-    GroupResourcesByPrefixHelper(const ResourceManager<Derived, T> &manager)
-        : mManager(manager)
-    {
-    }
-
-    void AddResource(const std::string &resourceID)
-    {
-        std::string prefix = NormalzeResourceID(resourceID);
-        mResourceMap[prefix].emplace_back(mManager.GetResource(resourceID));
-    }
-
-    ResourceMap<T> GetResources() { return mResourceMap; }
-
-private:
-    std::string NormalzeResourceID(const std::string &resourceID)
-    {
-        size_t underscorePos = resourceID.find_last_of('_');
-        if (underscorePos != std::string::npos)
-        {
-            bool isAllDigits = true;
-            for (size_t i = underscorePos + 1; i < resourceID.size(); ++i)
-            {
-                if (!std::isdigit(resourceID[i]))
-                {
-                    isAllDigits = false;
-                    break;
-                }
-            }
-
-            if (isAllDigits)
-            {
-                return resourceID.substr(0, underscorePos);
-            }
-        }
-        return resourceID;
-    }
-
-private:
-    const ResourceManager<Derived, T> &mManager;
-    ResourceMap<T> mResourceMap;
-};
+std::string NormalzeResourceID(const std::string &resourceID);
 
 template <typename Derived, typename T>
 ResourceMap<T> GroupResourcesByPrefix(const ResourceManager<Derived, T> &manager)
 {
-    GroupResourcesByPrefixHelper collector(manager);
+    ResourceMap<T> result;
     for (const std::string &resourceID : manager.GetResourceIDs())
     {
-        collector.AddResource(resourceID);
+        std::string prefix = NormalzeResourceID(resourceID);
+        result[prefix].emplace_back(manager.GetResource(resourceID));
     }
-    return collector.GetResources();
+    return result;
 }
